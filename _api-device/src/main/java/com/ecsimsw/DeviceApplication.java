2369a70ee9c59a0aee97a420c77e8befbbaa74ba
ecsimsw <ecsimsw@gmail.com>
2025-05-21 23:34:59 +0900

    feat : device module

---
+++ _api-device/src/main/java/com/ecsimsw/DeviceApplication.java
@@ -0,0 +1,13 @@
+package com.ecsimsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceApplication {
+
+    public static void main(String[] args) {
+        var app = new SpringApplication(DeviceApplication.class);
+        app.run(args);
+    }
+}
