682b77f783b5a085f535ab89c9f5f25743ecf4f3
ecsimsw <ecsimsw@gmail.com>
2025-06-10 22:47:34 +0900

    refactor : sdk

--- _api/event/src/main/java/com/ecsimsw/event/controller/DeviceOwnerController.java
+++ _api/event/src/main/java/com/ecsimsw/event/controller/DeviceOwnerController.java
@@ -3,7 +3,7 @@ package com.ecsimsw.event.controller;
 import com.ecsimsw.common.dto.ApiResponse;
 import com.ecsimsw.common.support.annotation.InternalHandler;
 import com.ecsimsw.event.service.DeviceOwnerService;
-import com.ecsimsw.sdkty.service.Platform1DeviceApiHandler;
+import com.ecsimsw.sdkty.service.PlatfromTyApiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DeviceOwnerController {
 
-    private final Platform1DeviceApiHandler platform1DeviceApiHandler;
+    private final PlatfromTyApiService platfromTyApiService;
     private final DeviceOwnerService deviceOwnerService;
 
     @InternalHandler
     @PostMapping("/api/event/beta/refresh/{username}")
     public ApiResponse<Void> refresh(@PathVariable String username) {
-        var deviceInfos = platform1DeviceApiHandler.getDeviceList(username);
+        var deviceInfos = platfromTyApiService.getDeviceList(username);
         deviceOwnerService.updateAll(username, deviceInfos);
         log.info("Refresh succeed : {}", username);
         return ApiResponse.success();
