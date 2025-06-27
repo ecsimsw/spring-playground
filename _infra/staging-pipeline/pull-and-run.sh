7faf1c3793bc4771b996cb50a45a1163d2095bed
ecsimsw <ecsimsw@gmail.com>
2025-06-27 11:28:51 +0900

    feat : update CI/CD script

--- _infra/staging-pipeline/pull-and-run.sh
+++ _infra/staging-pipeline/pull-and-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# ./pull-and-run.sh api-config 1.0.1 8850
+# ./pull-and-run.sh config 1.0.1 8850 prod
 
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 <MODULE_NAME> <VERSION> <EXTERNAL_PORT>"
-  echo "ex, $0 api-config 1.0.0 8850"
+if [ $# -ne 4 ]; then
+  echo "Usage: $0 <MODULE_NAME> <VERSION> <EXTERNAL_PORT> <PROFILE>"
+  echo "ex, $0 config 1.0.0 8850 prod"
   exit 1
 fi
 
@@ -12,6 +12,7 @@ REPO_NAME="spring-playground"
 MODULE_NAME="$1"
 VERSION="$2"
 PORT="$3"
+SPRING_PROFILE="$4"
 IMAGE_TAG="${MODULE_NAME}-${VERSION}"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 AWS_REGION=$(aws configure get region)
@@ -19,5 +20,9 @@ ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IM
 
 aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 docker pull "${ECR_URI}"
-docker run -d --rm --name "${MODULE_NAME}-${VERSION}" -p "${PORT}:${PORT}" "${ECR_URI}"
+docker run --rm \
+  --name "${MODULE_NAME}-${VERSION}" \
+  -p "${PORT}:${PORT}" \
+  -e SPRING_PROFILES_ACTIVE="${SPRING_PROFILE}" \
+  "${ECR_URI}"
 
